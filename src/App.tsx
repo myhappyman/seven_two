8be6cf54e1a88e674b4165bb0a534f5d7b983913
ParkShinwoo <myhappyman@naver.com>
2023-01-11 22:50:00 +0900

    three js 세팅후 첫 init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import Cylinder3d from "./Components/Cylinder3d";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="App-header">
+        <Canvas>
+          {/* <pointLight position={[10, 10, 10]} /> */}
+          {/* <ambientLight /> */}
+          <Cylinder3d position={[-1.2, 0, 0]} />
+          <Cylinder3d position={[1.2, 0, 0]} />
+        </Canvas>
+      </section>
+    </>
   );
 }
 
